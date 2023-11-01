14bf2514d26c4509e559db26338c39e39eb7ad9f
Guilherme Soares <soaresgui.dev@gmail.com>
2023-11-01 06:20:28 -0400

    feature(loginPage): added confirmationPage and toastify

--- src/features/authentication/AuthModal/form/ConfirmationRegisterForm/ConfirmationRegisterForm.tsx
+++ src/features/authentication/AuthModal/form/ConfirmationRegisterForm/ConfirmationRegisterForm.tsx
@@ -1,5 +1,17 @@
-/* TO DO */
+import './ConfirmationRegisterForm.css';
+import { IconContext } from "react-icons";
+import {BsPatchCheck} from 'react-icons/bs';
 
 function ConfirmationRegisterForm() {
+    return (
+        <>
+        <div id="confirmationRegister-container">
+            <IconContext.Provider value={{size: '3em'}}>
+                <BsPatchCheck/>
+            </IconContext.Provider>
+            <p id="confirmation-text"> We have send an confirmation email for your response. Please ensure to check your spam box incase you can’t find it in your inbox. </p>
+        </div>
+        </>
+    )
 
 } export default ConfirmationRegisterForm;
